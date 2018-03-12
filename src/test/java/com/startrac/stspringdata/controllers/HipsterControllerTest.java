378f357b3fa47d63cc3049b2e3f08f7f89947060
gn <gn@star-trac.de>
2018-03-12 16:54:04 +0100

    - added controller check

--- src/test/java/com/startrac/stspringdata/controllers/HipsterControllerTest.java
+++ src/test/java/com/startrac/stspringdata/controllers/HipsterControllerTest.java
@@ -1,15 +1,23 @@
 package com.startrac.stspringdata.controllers;
 
-import java.util.HashSet;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
-import com.startrac.stspringdata.model.Hipster;
+
 import com.startrac.stspringdata.services.HipsterService;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 public class HipsterControllerTest {
 
@@ -27,6 +35,12 @@ public class HipsterControllerTest {
         hipsterController = new HipsterController(hipsterService);
     }
 
+    @Test
+    public void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(hipsterController).build();
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+    }
+    
     @Test
     public void getIndexPage() {
         String returnPage = hipsterController.getIndexPage(model);
