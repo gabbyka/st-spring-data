c4b61ec1b1a8a3c7c8e106a6943b4cfa3534932b
gn <gn@star-trac.de>
2018-03-13 09:08:30 +0100

    - IT added

---
+++ src/test/java/com/startrac/stspringdata/repositories/HobbyRepositoryIT.java
@@ -0,0 +1,27 @@
+package com.startrac.stspringdata.repositories;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.startrac.stspringdata.model.Hobby;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class HobbyRepositoryIT {
+
+    @Autowired
+    HobbyRepository hobbyRepository;
+
+    @Test
+    public void findByName() throws Exception {
+        Optional<Hobby> current = hobbyRepository.findByName("travel");
+        assertEquals("travel", current.get().getName());
+    }
+}
